3ca3c4f28b799761a24c21036d8ddb5c243c667a
Shray Bansal <bansalshray@gmail.com>
2018-10-17 14:05:51 -0700

    pushing up sixthman-ci

--- bump_and_push.sh
+++ bump_and_push.sh
@@ -8,13 +8,13 @@ PACKAGE_VERSION=$(cat package.json | grep version | head -1 | awk -F: '{ print $
 # git tag v$PACKAGE_VERSION
 git push --tags
 
-$(aws ecr get-login --profile sixthman | sed 's/-e none//g')
+$(aws ecr get-login --profile sixthman-ci | sed 's/-e none//g')
 
 docker build -t 146006631841.dkr.ecr.us-west-1.amazonaws.com/docker-airflow:$PACKAGE_VERSION .
 docker push 146006631841.dkr.ecr.us-west-1.amazonaws.com/docker-airflow:$PACKAGE_VERSION
 
-sed -i -E "s/(docker-airflow:)([0-9.]+)/docker-airflow:$PACKAGE_VERSION/g" docker-compose-prod.yml
-sed -i -E "s/(docker-airflow:)([0-9.]+)/docker-airflow:$PACKAGE_VERSION/g" docker-compose-LocalExecutor.yml
+sed -i -r "s/(docker-airflow:)([0-9.]+)/docker-airflow:$PACKAGE_VERSION/g" docker-compose-prod.yml
+sed -i -r "s/(docker-airflow:)([0-9.]+)/docker-airflow:$PACKAGE_VERSION/g" docker-compose-LocalExecutor.yml
 
 git commit -m "$PACKAGE_VERSION"
 git push
