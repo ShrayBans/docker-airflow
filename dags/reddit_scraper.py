fe86e35468a0e33b94958a7bcd322a3b43c182a6
Shray Bansal <bansalshray@gmail.com>
2018-10-29 00:58:43 -0700

    changing dag to nba

--- dags/reddit_scraper.py
+++ dags/reddit_scraper.py
@@ -25,7 +25,7 @@ dag = DAG("reddit_scraper", default_args=default_args, schedule_interval=timedel
 t1 = BashOperator(
     task_id="scrape_nba",
     pool="reddit_scraper_nba",
-    bash_command="SUBREDDIT=lakers SCRAPING_MODE=latest node /usr/local/airflow/src/cheerio/reddit_scraper.js",
+    bash_command="DATABASE_API_CONNECTION=postgres://sixthman:lebrunsux123@sixthman-prod.cbdmxavtswxu.us-west-1.rds.amazonaws.com:5432/sixthman SUBREDDIT=nba SCRAPING_MODE=latest node /usr/local/airflow/src/cheerio/reddit_scraper.js",
     retries=3,
     dag=dag
  )
@@ -33,7 +33,7 @@ t1 = BashOperator(
 t2 = BashOperator(
     task_id="scrape_lakers",
     pool="reddit_scraper_lakers",
-    bash_command="SUBREDDIT=lakers SCRAPING_MODE=latest node /usr/local/airflow/src/cheerio/reddit_scraper.js",
+    bash_command="DATABASE_API_CONNECTION=postgres://sixthman:lebrunsux123@sixthman-prod.cbdmxavtswxu.us-west-1.rds.amazonaws.com:5432/sixthman SUBREDDIT=lakers SCRAPING_MODE=latest node /usr/local/airflow/src/cheerio/reddit_scraper.js",
     retries=3,
     dag=dag
  )
@@ -41,14 +41,14 @@ t2 = BashOperator(
 t3 = BashOperator(
     task_id="scrape_nbastreams",
     pool="reddit_scraper_nbastreams",
-    bash_command="SUBREDDIT=nbastreams SCRAPING_MODE=latest TIME_INTERVAL=5000 node /usr/local/airflow/src/cheerio/reddit_scraper.js",
+    bash_command="DATABASE_API_CONNECTION=postgres://sixthman:lebrunsux123@sixthman-prod.cbdmxavtswxu.us-west-1.rds.amazonaws.com:5432/sixthman SUBREDDIT=nbastreams SCRAPING_MODE=latest TIME_INTERVAL=5000 node /usr/local/airflow/src/cheerio/reddit_scraper.js",
     retries=3,
     dag=dag
  )
 t4 = BashOperator(
     task_id="find_nbastreams_link",
     pool="reddit_scraper_nbastreams",
-    bash_command="node /usr/local/airflow/src/cheerio/reddit_nbastreams_comments_scraper.js",
+    bash_command="DATABASE_API_CONNECTION=postgres://sixthman:lebrunsux123@sixthman-prod.cbdmxavtswxu.us-west-1.rds.amazonaws.com:5432/sixthman node /usr/local/airflow/src/cheerio/reddit_nbastreams_comments_scraper.js",
     retries=3,
     dag=dag
  )
