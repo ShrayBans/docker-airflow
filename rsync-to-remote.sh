41e546f143520c2d477bb82301a3263696843945
Shray Bansal <bansalshray@gmail.com>
2018-10-29 22:05:06 -0700

    adding node modules

--- rsync-to-remote.sh
+++ rsync-to-remote.sh
@@ -11,4 +11,5 @@ REMOTE_SERVER=$1
 # rsync -azvh docker-airflow $REMOTE_SERVER:docker-airflow # Only use to instantiate
 rsync -azvh dags $REMOTE_SERVER:docker-airflow/
 rsync -azvh src $REMOTE_SERVER:docker-airflow/
+rsync -azvh src $REMOTE_SERVER:docker-airflow/
 rsync -azvh docker-compose-prod.yml $REMOTE_SERVER:docker-airflow/
